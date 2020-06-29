13ec7dbee4eeec5187a6d310575d62cd38d2c502
Joao C Barbosa <joaao@outlook.com>
2020-06-29 15:19:11 -0300

    feat(sequelize): Update sequelize version from 5 to 6

---
+++ src/database/migrations/20200629174430-create-users.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING
+      }
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('users');
+  }
+};
