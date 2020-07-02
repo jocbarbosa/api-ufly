815b5a2fd2f9aa93795d8000a3fa7a7b483b9500
Joao C Barbosa <joaao@outlook.com>
2020-07-02 19:54:52 -0300

    feat: Add Country model

---
+++ src/models/Country.js
@@ -0,0 +1,18 @@
+const { Model, DataTypes } = require('sequelize');
+
+class Country extends Model {
+    static init(connection) {
+        super.init({
+            name: DataTypes.STRING,
+            flag_image: DataTypes.STRING,
+            image: DataTypes.STRING,
+            is_active: DataTypes.CHAR(1),
+            nationality: DataTypes.STRING
+        }, {
+            sequelize: connection,
+            tableName: 'countries'
+        })
+    }
+}
+
+module.exports = Country;
