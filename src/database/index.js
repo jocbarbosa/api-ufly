f1af2b1c67fc5da187704b9c0191dbea0ba3bab4
Joao C Barbosa <joaao@outlook.com>
2020-07-06 20:52:29 -0300

    refactor: Use requiredir to import models

--- src/database/index.js
+++ src/database/index.js
@@ -1,35 +1,25 @@
 const Sequelize = require('sequelize');
 const dbConfig = require('../config/database');
+const requireDir = require('require-dir');
 
-// Install requireDir to import all models
-
-// Instanciating Models
-const Country = require('../models/Country');
-const City = require('../models/City');
-const Manufacturer = require('../models/Manufacturer');
-const AircraftModel = require('../models/AircraftModel');
-const Owner = require('../models/Owner');
-const Aircraft = require('../models/Aircraft');
-const User = require('../models/User');
-const Flight = require('../models/Flight');
+const requiredModels = requireDir('../models');
 
 const connection = new Sequelize(dbConfig);
 
-
 // Start models and create associations
-Country.init(connection);
-City.init(connection);
-City.associate(connection.models);
-Manufacturer.init(connection);
-AircraftModel.init(connection);
-AircraftModel.associate(connection.models);
-Owner.init(connection);
-Aircraft.init(connection);
-Aircraft.associate(connection.models);
-User.init(connection);
-User.associate(connection.models);
-Flight.init(connection);
-Flight.associate(connection.models);
+requiredModels.Country.init(connection);
+requiredModels.City.init(connection);
+requiredModels.City.associate(connection.models);
+requiredModels.Manufacturer.init(connection);
+requiredModels.AircraftModel.init(connection);
+requiredModels.AircraftModel.associate(connection.models);
+requiredModels.Owner.init(connection);
+requiredModels.Aircraft.init(connection);
+requiredModels.Aircraft.associate(connection.models);
+requiredModels.User.init(connection);
+requiredModels.User.associate(connection.models);
+requiredModels.Flight.init(connection);
+requiredModels.Flight.associate(connection.models);
 
 
 module.exports = connection;
